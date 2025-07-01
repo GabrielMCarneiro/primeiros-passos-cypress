e7cf4ea8b3c1364e53d5d6e82afbeb50b52e4a73
Gabriel <gabrielmcarneiro17@gmail.com>
2025-07-01 19:13:00 -0300

    Atualização de cenário de usuário adicionada

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -7,16 +7,32 @@ describe('Orange HRM Tests', () => {
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dateField: "[placeholder='yyyy-dd-mm']",
+    submitButton: "[type='submit']",
   }
 
-  it('Login - Success', () => {
+  it.only('User Info Update - Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location("pathname").should("equal", "/web/index.php/dashboard/index")
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type("Benvilson")
+    cy.get(selectorsList.lastNameField).clear().type("Motta")
+    cy.get(selectorsList.genericField).eq(3).clear().type("Barabim")
+    cy.get(selectorsList.genericField).eq(4).clear().type("Barabum")
+    cy.get(selectorsList.genericField).eq(5).clear().type("Mister")
+    cy.get(selectorsList.dateField).eq(0).clear().type("2025-10-03")
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get("body").should("contain", "Successfully Updated")
+
   })
 
   it('Login - Fail', () => {
